faf37a5c423c6e460cec0afd8c6f06600cc16fa9
ivanov.v <ivanov.v@selectel.ru>
2021-05-26 13:30:25 +0300

    добавил плейбук в репу

---
+++ iface-get-static.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+iface1=$(ip a |grep mtu | grep -v lo |head -n1 | sed 's/\://g' | awk '{print $2}')
+cat > /target/etc/netplan/00-unstaller-config.yaml << EOF
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $iface1:
+      addresses:
+      - 192.168.1.25/24
+      gateway4: 192.168.1.1
+      nameservers:
+        addresses:
+        - 8.8.8.8
+        - 8.8.4.4
+EOF
+rm -f /target/etc/netplan/01-netcfg.yaml
