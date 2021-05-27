e20238aec4978af0547a7db3aba159204f626fe4
ivanov.v <ivanov.v@selectel.ru>
2021-05-27 16:24:54 +0300

    bios legacy готово

--- iface-get-static.sh
+++ iface-get-static.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 iface1=$(ip a |grep mtu | grep -v lo |head -n1 | sed 's/\://g' | awk '{print $2}')
+iface2=$(ip a |grep mtu | grep -v lo |tail -n1 | sed 's/\://g' | awk '{print $2}')
 cat > /etc/netplan/00-unstaller-config.yaml << EOF
 network:
   version: 2
@@ -13,5 +14,14 @@ network:
         addresses:
         - 8.8.8.8
         - 8.8.4.4
+    $iface2:
+      addresses:
+      - 10.10.0.2/24
+      gateway4: 10.10.0.1
+      nameservers:
+        addresses:
+        - 8.8.8.8
+        - 8.8.4.4
 EOF
 rm -f /etc/netplan/01-netcfg.yaml
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
--- wget-unzip-netboot.sh
+++ wget-unzip-netboot.sh
@@ -2,4 +2,8 @@
 wget http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/legacy-images/netboot/netboot.tar.gz -O /var/lib/tftpboot/netboot.tar.gz
 cd /var/lib/tftpboot/
 tar xvzf ./netboot.tar.gz
-#wget http://old-releases.ubuntu.com/releases/20.04/ubuntu-20.04-live-server-amd64.iso -O /var/lib/tftpboot/
+#wget http://old-releases.ubuntu.com/releases/20.04/ubuntu-20.04-live-server-amd64.iso -O /var/lib/tftpboot/ubuntu-20.04-live-server-amd64.iso
+#mount /var/lib/tftpboot/ubuntu-20.04-live-server-amd64.iso /mnt
+#cp /mnt/casper/vmlinuz /var/lib/tftpboot/
+#cp /mnt/casper/initrd /var/lib/tftpboot/
+#umount /mnt
